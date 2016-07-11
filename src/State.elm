54f7ac22e98640a2e10319d50ebc85a49f330f6f
Tom <todavies5@gmail.com>
2016-07-11 18:56:07 +0100

    Fix missing import

--- src/State.elm
+++ src/State.elm
@@ -1,5 +1,7 @@
 module State exposing (..)
 
+import Types exposing (..) -- Make Types module visible in this file
+
 init : ( Model, Cmd Msg ) -- "Cmd" means this is a way of describing side-effects
 init = 
     ( {}
