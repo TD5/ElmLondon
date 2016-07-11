0b7d61cb88f8e0c7b7986739ed9c4d15e86c7be0
Tom <todavies5@gmail.com>
2016-07-11 19:37:39 +0100

    Add Rest module with getNews func

---
+++ src/Rest.elm
@@ -0,0 +1,7 @@
+module Rest exposing (..)
+
+import Types exposing (..)
+
+getNews : Cmd Msg
+getNews =
+    "https://hn/algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=50"
--- src/State.elm
+++ src/State.elm
@@ -1,12 +1,13 @@
 module State exposing (..)
 
 import Types exposing (..) -- Make Types module visible in this file
+import Rest exposing (..) 
 
 
 init : ( Model, Cmd Msg ) -- "Cmd" means this is a way of describing side-effects
 init = 
     ( { news = [] }
-    , Cmd.none 
+    , getNews -- From imported Rest
     )
 
 
