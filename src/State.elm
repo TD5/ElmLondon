71c20dd43aa21845a7f80da7d877a8d2ead63ab0
Tom <todavies5@gmail.com>
2016-07-11 19:20:07 +0100

    Render model as toString

--- src/State.elm
+++ src/State.elm
@@ -2,18 +2,21 @@ module State exposing (..)
 
 import Types exposing (..) -- Make Types module visible in this file
 
+
 init : ( Model, Cmd Msg ) -- "Cmd" means this is a way of describing side-effects
 init = 
     ( {}
     , Cmd.none 
     )
 
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp -> ( model, Cmd.none )
 
-subscriptions : Model -> Sub Msg -- WHat should I be listening for?
+
+subscriptions : Model -> Sub Msg -- What should I be listening for?
 subscriptions model =
     Sub.none
 
--- src/Types.elm
+++ src/Types.elm
@@ -1,7 +1,9 @@
 module Types exposing (..) -- Everything
 
+
 type alias Model =
     {}
+
 
 type Msg =
     NoOp
